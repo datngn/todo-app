5badbedf0507365a9b1658e8559810ee5c8d9b53
Datngn <nguyentiendat2609@gmail.com>
2022-08-02 16:27:54 +0700

    Updates

---
+++ precache-manifest.ae5cfaa2c1a5d593e12409609c3cb501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f8e0d30bd1be331f772",
+    "url": "./static/css/main.9979e808.chunk.css"
+  },
+  {
+    "revision": "8f8e0d30bd1be331f772",
+    "url": "./static/js/main.8f8e0d30.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "./static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "bb6df967e226d3d1baa5",
+    "url": "./static/js/2.bb6df967.chunk.js"
+  },
+  {
+    "revision": "bf097dc6f982748a6327",
+    "url": "./static/js/runtime~main.bf097dc6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "90830af0b40dea7eb2e5f57043db69b6",
+    "url": "./index.html"
+  }
+];
